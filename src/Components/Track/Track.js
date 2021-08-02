ae4401b23ea91c842cd47d2df41935ddada57826
Petrus Rantamäki <Petrus Rantamki>
2021-08-02 12:17:36 +0300

    add searchResults state

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -3,19 +3,20 @@ import './Track.css';
 
 class Track extends React.Component {
   renderAction() {
-    if(isRemoval) {
-      return '-';
+    if(this.props.isRemoval) {
+      return <button className="Track-action">-</button>
+    } else {
+      return <button className="Track-action">+</button>
     }
-    return '+';
   }
   render() {
     return (
       <div className="Track">
         <div className="Track-information">
-          <h3>track name</h3>
-          <p>track artist | track album</p>
+          <h3>{this.props.track.name}</h3>
+          <p>{this.props.track.artist} | {this.props.track.album}</p>
         </div>
-        <button className="Track-action">{this.renderAction}</button>
+        <button className="Track-action">{this.renderAction()}</button>
       </div>
     );
   }

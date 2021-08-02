f3ea68da4723910cb56df1ab4f05bc370a06d890
Petrus Rantamäki <Petrus Rantamki>
2021-08-02 14:23:12 +0300

    add playlist objects

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,7 +10,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state= {
-      searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, {name: 'name3', artist: 'artist3', album: 'album3', id: 3}]
+      searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
+      playlistName: '',
+      playlistTracks: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
     };
   }
   render() {
@@ -21,7 +23,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} />
-            <Playlist />
+            <Playlist playlistName={this.state.playlistName}  playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>

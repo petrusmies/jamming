50c1ae757b5e3a5ceb60ffe7cbbc32c7d4fb49be
Petrus Rantamäki <Petrus Rantamki>
2021-08-03 13:08:28 +0300

    add search bar methods

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -19,6 +19,7 @@ class App extends React.Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -47,12 +48,16 @@ class App extends React.Component {
     console.log(trackURIs);
   }
 
+  search(term) {
+    console.log(term);
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults onAdd={this.addTrack}
                            searchResults={this.state.searchResults} />

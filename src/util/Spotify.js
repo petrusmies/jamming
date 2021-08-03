155eb8f75b4e1fe5204db7246994707a338e44d7
Petrus Rantamäki <Petrus Rantamki>
2021-08-03 14:17:25 +0300

    set spotify client id etc.

---
+++ src/util/Spotify.js
@@ -0,0 +1,27 @@
+let accessToken;
+const clientID = '88fc3c39e32541d88fb2abab2c7851b6';
+const redirectURI = 'http://localhost:3000/';
+
+const Spotify = {
+  getAccessToken() {
+    if(accessToken) {
+      return accessToken;
+    }
+
+    // Make sure the acces token is present
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if(accessToken && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      expiresIn = Number(expiresInMatch[1]);
+
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      window.location(`https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`);
+      return accessToken;
+    }
+  }
+}
+
+export default Spotify;
